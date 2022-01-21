593e295d27c9fd4388f5d158bfde8e212fe54405
Daniel Palmer <daniel@0x0f.com>
2022-01-22 01:46:15 +0900

    Actually open the GE dev

--- libge.c
+++ libge.c
@@ -1,13 +1,26 @@
 // SPDX-License-Identifier:	LGPL-2.1+
+#include <fcntl.h>
+#include <errno.h>
+#include <unistd.h>
 
 #include "libge.h"
 
 int libge_open(struct ge_cntx *ge)
 {
+	int fd;
+
+	fd = open("/dev/mstar-ge", O_RDWR);
+	if (fd < 0)
+		return errno;
+
+	ge->fd = fd;
+
 	return 0;
 }
 
 int libge_close(struct ge_cntx *ge)
 {
+	close(ge->fd);
+
 	return 0;
 }
