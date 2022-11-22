e837003f83ef4114a896286854b2e7738e3a6c4d
Daniel Palmer <daniel@0x0f.com>
2022-11-22 20:22:01 +0900

    Helps to install the library :)

--- libge.c
+++ libge.c
@@ -11,7 +11,6 @@
 int libge_open(struct ge_cntx *ge)
 {
 	int fd, ret;
-	struct ge_info info;
 
 	fd = open("/dev/mstar-ge", O_RDWR);
 	if (fd < 0)
@@ -19,24 +18,72 @@ int libge_open(struct ge_cntx *ge)
 
 	ge->fd = fd;
 
-	ret = ioctl(ge->fd, LIBGE_IOCTL_INFO, &info);
+	ret = ioctl(ge->fd, MSTAR_GE_IOCTL_INFO, &ge->info);
 	if (ret)
 		goto out;
-
-	memcpy(&ge->info, &info, sizeof(info));
 
 out:
 	return ret;
 }
 
-int libge_queue_job(struct ge_cntx *ge, struct ge_opdata *opdata, uint32_t *tag)
+int libge_queue_job(struct ge_cntx *ge,
+					struct mstar_ge_opdata *opdata,
+					unsigned int num_ops,
+					int src_fd,
+					int src_w, int src_h, __u32 src_fourcc, unsigned int src_pitch,
+					int dst_fd,
+					int dst_w, int dst_h, __u32 dst_fourcc, unsigned int dst_pitch,
+					unsigned long *tag)
 {
-	return 0;
+	struct mstar_ge_job_request req;
+	struct mstar_ge_buf bufs[2];
+	int ret, num_buf = 0;
+
+	if (src_fd > 0) {
+		bufs[num_buf].fd = src_fd;
+		bufs[num_buf].cfg.width = src_w;
+		bufs[num_buf].cfg.height = src_h;
+		bufs[num_buf].cfg.fourcc = src_fourcc;
+		bufs[num_buf].cfg.pitch = src_pitch;
+		num_buf++;
+	}
+
+	if (dst_fd > 0) {
+		bufs[num_buf].fd = dst_fd;
+		bufs[num_buf].cfg.width = dst_w;
+		bufs[num_buf].cfg.height = dst_h;
+		bufs[num_buf].cfg.fourcc = dst_fourcc;
+		bufs[num_buf].cfg.pitch = dst_pitch;
+		num_buf++;
+	}
+
+	if (num_buf == 0)
+		return -EINVAL;
+
+	req.tag = tag;
+	req.ops = opdata;
+	req.num_ops = num_ops;
+	req.bufs = &bufs;
+	req.num_bufs = num_buf;
+
+	ret = ioctl(ge->fd, MSTAR_GE_IOCTL_QUEUE, &req);
+	if (ret)
+		goto out;
+
+out:
+	return ret;
 }
 
-int libge_query_job(struct ge_cntx *ge, uint32_t tag)
+int libge_query_job(struct ge_cntx *ge, unsigned long tag)
 {
-	return 0;
+	int ret;
+
+	ret = ioctl(ge->fd, MSTAR_GE_IOCTL_QUERY, &tag);
+	if (ret)
+		goto out;
+
+out:
+	return ret;
 }
 
 int libge_close(struct ge_cntx *ge)
--- libge.h
+++ libge.h
@@ -1,43 +1,91 @@
 // SPDX-License-Identifier:	LGPL-2.1+
 
-#define LIBGE_IOCTL_INFO	0
-
-/* Probing */
-struct ge_info {
-	uint32_t caps;
-};
+#include <linux/types.h>
+#include <drm/mstar_ge.h>
 
 struct ge_cntx {
 	int fd;
-	struct ge_info info;
-};
-
-/* The meat and potatoes */
-enum ge_op {
-	GE_OP_BITBLT,
-};
-
-#define GE_ROTATION_0	0
-#define GE_ROTATION_90	1
-#define GE_ROTATION_180	2
-#define GE_ROTATION_270	3
-
-struct ge_bitblt {
-	unsigned int rotation;
-};
-
-struct ge_opdata {
-	enum ge_op op;
-	void *src, *dst;
-	uint32_t src_width, src_height, dst_width, dst_height;
-	uint32_t src_pitch, dst_pitch;
-	uint32_t src_fourcc, dst_fourcc;
-	union {
-		struct ge_bitblt bitblt;
-	};
+	struct mstar_ge_info info;
 };
 
 int libge_open(struct ge_cntx *ge);
-int libge_queue_job(struct ge_cntx *ge, struct ge_opdata *opdata, uint32_t *tag);
-int libge_query_job(struct ge_cntx *ge, uint32_t tag);
+int libge_queue_job(struct ge_cntx *ge,
+					struct mstar_ge_opdata *opdata,
+					unsigned int num_ops,
+					int src_fd,
+					int src_w, int src_h, __u32 src_fourcc, unsigned int src_pitch,
+					int dst_fd,
+					int dst_w, int dst_h, __u32 dst_fourcc, unsigned int dst_pitch,
+					unsigned long *tag);
+int libge_query_job(struct ge_cntx *ge, unsigned long tag);
 int libge_close(struct ge_cntx *ge);
+
+static inline void libge_filljob_fillrect(struct mstar_ge_opdata *opdata,
+										  unsigned int x0,
+										  unsigned int y0,
+										  unsigned int x1,
+										  unsigned int y1,
+										  unsigned int a,
+										  unsigned int r,
+										  unsigned int g,
+										  unsigned int b)
+{
+	opdata->op = MSTAR_GE_OP_RECTFILL;
+
+	opdata->rectfill.x0 = x0;
+	opdata->rectfill.y0 = y0;
+	opdata->rectfill.x1 = x1;
+	opdata->rectfill.y1 = y1;
+
+	opdata->rectfill.start_color.a = a;
+	opdata->rectfill.start_color.r = r;
+	opdata->rectfill.start_color.g = g;
+	opdata->rectfill.start_color.b = b;
+}
+
+static inline void libge_filljob_blit(struct mstar_ge_opdata *opdata,
+								      unsigned int src_x0,
+								      unsigned int src_y0,
+									  unsigned int dst_x0,
+									  unsigned int dst_y0,
+								      unsigned int dst_x1,
+								      unsigned int dst_y1,
+									  enum mstar_ge_rotation rotation)
+{
+	opdata->op = MSTAR_GE_OP_BITBLT;
+
+	opdata->bitblt.src_x0 = src_x0;
+	opdata->bitblt.src_y0 = src_y0;
+	opdata->bitblt.dst_x0 = dst_x0;
+	opdata->bitblt.dst_y0 = dst_y0;
+	opdata->bitblt.dst_x1 = dst_x1;
+	opdata->bitblt.dst_y1 = dst_y1;
+
+	opdata->bitblt.rotation = rotation;
+}
+
+static inline void libge_filljob_strblit(struct mstar_ge_opdata *opdata,
+								      unsigned int src_x0,
+								      unsigned int src_y0,
+									  unsigned int src_x1,
+									  unsigned int src_y1,
+									  unsigned int dst_x0,
+									  unsigned int dst_y0,
+								      unsigned int dst_x1,
+								      unsigned int dst_y1,
+									  enum mstar_ge_rotation rotation)
+{
+	opdata->op = MSTAR_GE_OP_STRBLT;
+
+	opdata->strblt.src_x0 = src_x0;
+	opdata->strblt.src_y0 = src_y0;
+	opdata->strblt.src_x1 = src_x1;
+	opdata->strblt.src_y1 = src_y1;
+
+	opdata->strblt.dst_x0 = dst_x0;
+	opdata->strblt.dst_y0 = dst_y0;
+	opdata->strblt.dst_x1 = dst_x1;
+	opdata->strblt.dst_y1 = dst_y1;
+
+	opdata->strblt.rotation = rotation;
+}
